9a4bf50f28d0312d436a559aae79f783e20259ec
EvanOberholster <eroberholster@gmail.com>
2018-03-29 14:48:46 -0300

    updates
    Not working

--- dlib-go.h
+++ dlib-go.h
@@ -1,24 +1,39 @@
-#include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing.h>
-#include <dlib/image_io.h>
+#include "dlib/dlib/image_processing/frontal_face_detector.h"
+#include "dlib/dlib/image_processing.h"
+#include "dlib/dlib/image_io.h"
+#include "dlib/dlib/image_loader/jpeg_loader.h"
+#include "dlib/dlib/image_loader/png_loader.h"
 #include <iostream>
 
-namespace dlib {
+using namespace dlib;
+using namespace std;
 
-
-full_object_detection ProcessRect(const std::string dfilename, const std::string ifilename, const rectangle rect) {
-    frontal_face_detector detector = get_frontal_face_detector();
-    shape_predictor sp;
-
+dlib::shape_predictor LoadShapePredictor(const std::string& dfilename) {
+    dlib::shape_predictor sp;
     deserialize(dfilename) >> sp;
+    return sp;
+};
 
-    array2d<rgb_pixel> img;
-    load_image(img, ifilename);
+dlib::full_object_detection UseShapePredictor(const dlib::shape_predictor& sp, const std::string& ifilename, const dlib::rectangle rect) {
+    dlib::array2d<dlib::rgb_pixel> img;
+    dlib::load_image(img, ifilename);
 
-    std::vector<rectangle> dets = detector(img);
-
-    full_object_detection shape = sp(img, rect);
+    dlib::full_object_detection shape = sp(img, rect);
     return shape;
 };
 
-}
+dlib::full_object_detection DetectObjectsRect(const std::string& dfilename, const std::string& ifilename, const dlib::rectangle rect) {
+    dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+    dlib::shape_predictor sp;
+
+    deserialize(dfilename) >> sp;
+    // dlib::deserialize(dfilename) >> sp;
+
+    dlib::array2d<dlib::rgb_pixel> img;
+    dlib::load_image(img, ifilename);
+
+    std::vector<dlib::rectangle> dets = detector(img);
+
+    dlib::full_object_detection shape = sp(img, rect);
+    return shape;
+};
--- example/main.go
+++ example/main.go
@@ -7,8 +7,23 @@ import(
 
 func main() {
   log.Printf("Example Create Rectangle")
-  rect := detector.Centered_rect__SWIG_0(1,1,5,10)
+  rect := detector.Centered_rect__SWIG_0(30,30,300,300)
 
   log.Printf("Example Caclulate Rectangle Area")
   log.Println(detector.Area(rect))
+
+  shapePred := detector.LoadShapePredictor("/go/src/detector/example/shape_predictor_68_face_landmarks.dat")
+
+  pred := detector.UseShapePredictor(shapePred, "/go/src/detector/example/example.jpg", rect)
+  log.Println(pred)
+
+  pred2 := detector.UseShapePredictor(shapePred, "/go/src/detector/example/example.jpg", rect)
+  log.Println(pred2)
+
+  // detector filename, image filename, rectangle
+  res := detector.DetectObjectsRect("shape_predictor_68_face_landmarks.dat", "/go/src/detector/example/example.jpg", rect)
+
+
+  log.Println(res)
+
 }
